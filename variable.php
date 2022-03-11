2f07e69798efffb313d08521b40b625d19727a67
dickyardianto08 <dickyard08@gmail.com>
2022-03-11 18:29:03 +0700

    Mengupload file variable.php

---
+++ variable.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Variable</title>
+</head>
+<body>
+<?php 
+
+$angka1 = 10 ;
+$angka2 = 5 ;
+
+$hasiladd = $angka1 + $angka2 ;
+$hasilmin = $angka1 - $angka2 ;
+$hasilmult = $angka1 * $angka2 ;
+
+echo "Hasil Penjumlahan = <br> ".$angka1.'+'.$angka2. '='. $hasiladd ;
+echo "<br> Hasil Pengurangan = <br> ".$angka1.'-'.$angka2. '='. $hasilmin ;
+echo "<br> Hasil Perkalian = <br> ".$angka1.'*'.$angka2. '='. $hasilmult ;
+
+ ?>
+</body>
+</html>
