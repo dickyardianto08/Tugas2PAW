d425b0b90fccd32db569e721f2595ce7d85a5ab5
dickyardianto08 <dickyard08@gmail.com>
2022-03-11 18:27:58 +0700

    Mengupload file hello.php

---
+++ hello.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Hello</title>
+</head>
+<body>
+	<?php 
+	echo "Nama : Dicky Ardianto <br>" ; //Nama
+	echo "NIM : 200411100064" ; // NIM
+ 	?>
+</body>
+</html>
